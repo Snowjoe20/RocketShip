dc5a1d68eef411f7cc5382a425858a4a8d28c587
Snowjoe20 <36928522+Snowjoe20@users.noreply.github.com>
2018-03-07 10:57:36 -0600

    Add files via upload

---
+++ RocketShip.py
@@ -0,0 +1,22 @@
+from Graphics import *
+
+class Rocket(object):
+    def __init__(self, x, y, color, win):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.buildBooster(x,y, color, win)
+
+
+class Capsule(object):
+    def __init__(self):()
+
+
+
+
+class RocketShip(object):
+    def __init__(self):
+        cap = Capsule
+        boost1 = Rocket
+        boost2 = Rocket
+
